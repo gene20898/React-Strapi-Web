beaef6473df68d999729b99f6449c710e68614ec
Wayu Bangkamed <gene20898@hotmail.com>
2022-09-15 21:22:11 +0700

    add bloglist

--- next-trs-app/components/news/BlogList.js
+++ next-trs-app/components/news/BlogList.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { emphasize, fade, darken, lighten } from '@material-ui/core/styles/colorManipulator';
 
 import clsx from 'clsx';
 
@@ -9,9 +8,9 @@ import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
-import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
 import { useTheme } from '@material-ui/core/styles';
-
+import { getStrapiMedia } from '@lib/media';
 
 const useStyles = makeStyles((theme) => ({
   post: {
@@ -20,9 +19,17 @@ const useStyles = makeStyles((theme) => ({
       marginBottom: 0
     }
   },
-  img: {
-    maxWidth: '100%',
+  thumbnail: {
+    maxWidth: "100%",
+    height: "40%",
+    display: "inline-block", 
+    objectFit: "cover",
+    backgroundPosition: "center center",
     borderRadius: theme.shape.borderRadius
+  },
+  img: {
+    borderRadius: theme.shape.borderRadius,
+    maxWidth: '100%',
   },
   bullet: {
     marginLeft: theme.spacing(1),
@@ -35,16 +42,16 @@ const useStyles = makeStyles((theme) => ({
 }
 ));
 
-export default function Component(props) {
+export default function BlogList( {articles} ) {
   const classes = useStyles();
-  const theme = useTheme();
-  return (
+    return (
 <section>
   <Container maxWidth="lg">
     <Box py={10}>
       <Grid container spacing={3}>
+        {articles.map((article) => (
         <Grid item xs={12} md={4} className={classes.post}>
-          <img src="https://images.unsplash.com/photo-1493397212122-2b85dda8106b?ixlib=rb-1.2.1&auto=format&fit=crop&w=960&q=80" alt="" className={classes.img} />
+          <img src={ getStrapiMedia(article.attributes.thumbnail)} alt="" className={classes.img} />
           <Box mt={2} mb={2}>
             <Box mb={1}>
               <Typography variant="overline" component="span" color="textSecondary">24 Jan, 2020</Typography>
@@ -55,31 +62,11 @@ export default function Component(props) {
           </Box>
           <Typography variant="subtitle1" paragraph={true} color="textSecondary">Suspendisse aliquam tellus ante, porttitor mattis diam eleifend quis. Pellentesque pulvinar commodo eros sit amet finibus. Aenean et ornare erat.</Typography>
         </Grid>
-        <Grid item xs={12} md={4} className={classes.post}>
-          <img src="https://images.unsplash.com/photo-1511818966892-d7d671e672a2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=960&q=80" alt="" className={classes.img} />
-          <Box mt={2} mb={2}>
-            <Box mb={1}>
-              <Typography variant="overline" component="span" color="textSecondary">24 Jan, 2020</Typography>
-            </Box>
-            <Link href="#" color="inherit">
-              <Typography variant="h5" component="h2">Lorem ipsum dolor sit amet consectetur.</Typography>
-            </Link>
-          </Box>
-          <Typography variant="subtitle1" paragraph={true} color="textSecondary">Suspendisse aliquam tellus ante, porttitor mattis diam eleifend quis. Pellentesque pulvinar commodo eros sit amet finibus. Aenean et ornare erat.</Typography>
-        </Grid>
-        <Grid item xs={12} md={4} className={classes.post}>
-          <img src="https://images.unsplash.com/photo-1451976426598-a7593bd6d0b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=960&q=80" alt="" className={classes.img} />
-          <Box mt={2} mb={2}>
-            <Box mb={1}>
-              <Typography variant="overline" component="span" color="textSecondary">24 Jan, 2020</Typography>
-            </Box>
-            <Link href="#" color="inherit">
-              <Typography variant="h5" component="h2">Lorem ipsum dolor sit amet consectetur.</Typography>
-            </Link>                
-          </Box>
-          <Typography variant="subtitle1" paragraph={true} color="textSecondary">Suspendisse aliquam tellus ante, porttitor mattis diam eleifend quis. Pellentesque pulvinar commodo eros sit amet finibus. Aenean et ornare erat.</Typography>
-        </Grid>
+        ))}
       </Grid>
+    </Box>
+    <Box pb={10} display="flex" justifyContent="center" alignItems="center">
+      <Button variant="outlined" color="primary">See more</Button>
     </Box>
   </Container>
 </section>
--- next-trs-app/pages/news/index.js
+++ next-trs-app/pages/news/index.js
@@ -1,21 +1,39 @@
 import React from 'react';
+import { useState } from 'react';
 
 import Metatags from '@components/Metatags';
 import Banner from '@components/Banner';
 import BlogList from '@components/news/BlogList';
-const meta = {}; 
+import { fetchAPI } from '@lib/api';
 
-export default function News() {
+const PAGE_SIZE = 9;
+
+export default function News({ articles }) {
   return (
     <main>
       <Metatags title="Home Page" description="Get the latest posts on our site" />
-
+      {console.log(articles)}
       <Banner />
-
-      <BlogList />
-      <BlogList />
-      <BlogList />
+      <BlogList articles={articles}/>
     </main>
   );
+}
+
+export async function getStaticProps() {
+  // Run API calls in parallel
+  const articlesRes = await fetchAPI("/articles", { 
+    pagination: {
+      page: 1,
+      pageSize: PAGE_SIZE,
+    },
+    sort: ['createdAt', 'updatedAt'],
+    populate: "thumbnail" 
+  });
+
+  return {
+    props: {
+      articles: articlesRes.data,
+    }
+  };
 }
 
