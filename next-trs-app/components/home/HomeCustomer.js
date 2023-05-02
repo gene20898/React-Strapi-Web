586e94196ef1930b2d99479b13a1916850ee273a
Wayu Bangkamed <wayu.bangkamed@gmail.com>
2023-05-02 12:02:02 +0700

    Link customer and portfolio section

--- next-trs-app/components/home/HomeCustomer.js
+++ next-trs-app/components/home/HomeCustomer.js
@@ -1,15 +1,12 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { emphasize, fade, darken, lighten } from '@material-ui/core/styles/colorManipulator';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 
-import clsx from 'clsx';
-
-import Container from '@material-ui/core/Container';
-import Box from '@material-ui/core/Box';
-import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
-import { useTheme } from '@material-ui/core/styles';
-
+import Container from "@material-ui/core/Container";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import { useTheme } from "@material-ui/core/styles";
+import { getStrapiMedia } from "@lib/media";
 
 const useStyles = makeStyles((theme) => ({
   logoWrapper: {
@@ -18,68 +15,62 @@ const useStyles = makeStyles((theme) => ({
   logo: {
     height: 48,
     marginTop: theme.spacing(4),
-    marginBottom: theme.spacing(4)
+    marginBottom: theme.spacing(4),
   },
-}
-));
+}));
 
-export default function Component(props) {
+export default function Component({ customer }) {
   const classes = useStyles();
   const theme = useTheme();
   return (
-<section>
-  <div style={{backgroundColor:theme.palette.common.white, paddingBottom: theme.spacing(4)}}>
-    <Container maxWidth="lg">
-      <Box py={3}>
-        <Box mb={2}>
-          <Typography variant="h3" component="h3" align="center" gutterBottom={true} color="primary" >Our customers</Typography> 
-          <Typography variant="overline" component="h3" align="center" color="primary">LOREM IPSUM DOLOR SIT AMET CONSECTUTAR</Typography>
-        </Box>
-        <Grid container spacing={2}>
-          <Grid item xs={6} sm={3}>
-            <Box display="flex" justifyContent="center" className={classes.logoWrapper}>
-              <img src="nereus-assets/img/logos/apple.svg" alt="" className={classes.logo} />
+    <section>
+      <div
+        style={{
+          backgroundColor: theme.palette.common.white,
+          paddingBottom: theme.spacing(4),
+        }}
+      >
+        <Container maxWidth="lg">
+          <Box py={3}>
+            <Box mb={2}>
+              <Typography
+                variant="h3"
+                component="h3"
+                align="center"
+                gutterBottom={true}
+                color="primary"
+              >
+                {customer?.title}
+              </Typography>
+              <Typography
+                variant="overline"
+                component="h3"
+                align="center"
+                color="primary"
+              >
+                {customer?.description}
+              </Typography>
             </Box>
-          </Grid>
-          <Grid item xs={6} sm={3}>
-            <Box display="flex" justifyContent="center" className={classes.logoWrapper}>
-              <img src="nereus-assets/img/logos/facebook.svg" alt="" className={classes.logo} />
-            </Box>
-          </Grid>
-          <Grid item xs={6} sm={3}>
-            <Box display="flex" justifyContent="center" className={classes.logoWrapper}>
-              <img src="nereus-assets/img/logos/marvel.svg" alt="" className={classes.logo} />
-            </Box>
-          </Grid>
-          <Grid item xs={6} sm={3}>
-            <Box display="flex" justifyContent="center" className={classes.logoWrapper}>
-              <img src="nereus-assets/img/logos/react.svg" alt="" className={classes.logo} />
-            </Box>
-          </Grid>
-          <Grid item xs={6} sm={3}>
-            <Box display="flex" justifyContent="center" className={classes.logoWrapper}>
-              <img src="nereus-assets/img/logos/nike.svg" alt="" className={classes.logo} />
-            </Box>
-          </Grid>
-          <Grid item xs={6} sm={3}>
-            <Box display="flex" justifyContent="center" className={classes.logoWrapper}>
-              <img src="nereus-assets/img/logos/airbnb.svg" alt="" className={classes.logo} />
-            </Box>
-          </Grid>
-          <Grid item xs={6} sm={3}>
-            <Box display="flex" justifyContent="center" className={classes.logoWrapper}>
-              <img src="nereus-assets/img/logos/php.svg" alt="" className={classes.logo} />
-            </Box>
-          </Grid>
-          <Grid item xs={6} sm={3}>
-            <Box display="flex" justifyContent="center" className={classes.logoWrapper}>
-              <img src="nereus-assets/img/logos/tesla.svg" alt="" className={classes.logo} />
-            </Box>
-          </Grid>
-        </Grid>
-      </Box>
-    </Container>
-  </div>
-</section>
+            <Grid container spacing={2} justifyContent="center">
+              {customer?.logo.map((logo) => (
+                <Grid item xs={6} sm={3}>
+                  <Box
+                    display="flex"
+                    justifyContent="center"
+                    className={classes.logoWrapper}
+                  >
+                    <img
+                      src={getStrapiMedia(logo.logo)}
+                      alt=""
+                      className={classes.logo}
+                    />
+                  </Box>
+                </Grid>
+              ))}
+            </Grid>
+          </Box>
+        </Container>
+      </div>
+    </section>
   );
 }
